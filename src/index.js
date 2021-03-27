aad2855f99d927e6ffb4163085a890b88fb9e417
unknown <aromero@aeonlinesolutions.com>
2021-03-27 13:19:18 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hola desde React!!</h1>
+
+const container = document.getElementById('app');
+
+// ReactDOM.render(__que__, __donde__);
+ReactDOM.render(element, container);
